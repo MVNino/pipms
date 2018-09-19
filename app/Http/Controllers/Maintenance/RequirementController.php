bc32c291e915c16eee23bcb2fc845c43ffb51f66
Marlon Nino <me@ninomarlonvilla@gmail.com>
2018-09-19 23:56:22 +0800

    building requirements maintenance

--- app/Http/Controllers/Maintenance/RequirementController.php
+++ app/Http/Controllers/Maintenance/RequirementController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\Maintenance;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Requirement;
 
 class RequirementController extends Controller
 {
 	public function maintainRequirements()
 	{
-		return view('admin.maintenance.requirement');
+		$requirements = Requirement::all();
+		return view('admin.maintenance.requirement', 
+				['requirements' => $requirements]);
+	}
+
+	public function addRequirement(Request $request)
+	{
+		$this->validate($request, [
+			'txtAreaRequirement' => 'required',
+			'radioIPR' => 'required'
+		]);
+
+		$requirement = new Requirement;
+		$requirement->str_requirement = $request->txtAreaRequirement;
+		$requirement->char_ipr = $request->radioIPR;
+		if ($requirement->save()) {
+			return redirect()->back()->with('success', 'Requirement added!');
+		}
 	}
 }
