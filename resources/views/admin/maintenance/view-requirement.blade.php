3f3d303ee4083aa18fc74d90585719eccc47aa94
Marlon Nino <me@ninomarlonvilla@gmail.com>
2018-10-05 04:47:22 +0800

    build view requirement maintenance

---
+++ resources/views/admin/maintenance/view-requirement.blade.php
@@ -0,0 +1,80 @@
+@extends('admin.layouts.app')
+
+@section('pg-title')
+<h1><i class="fa fa-file"></i> View Requirement</h1>
+  <p>A requirement in application for copyright/patent registration</p>
+@endsection
+@section('breadcrumb-label')
+<li class="breadcrumb-item">Maintenance</li>
+<li class="breadcrumb-item"><a href="/admin/maintenance/requirements">Requirements</a></li>
+<li class="breadcrumb-item"><a href="/admin/maintenance/requirement/{id}">View Requirement</a></li>
+@endsection
+@section('content')
+  <h3>DEV HERE! query ka sa controller nito, tapos ang content dapat ay 
+    ung related sa requirement(requirement itself, 
+    tapos i show ung other requirements na ka 
+    classification niya(example: copyright) sa right side section.. 
+    tapos dapat pwedeng iedit ung requirement record na to(modal)</h3>
+    <p>basta copy paste ka nalang sa content ng ibang maintenance dito..
+      paggagayahan mo ng hitsura & structure kumbaga</p>
+@endsection
+
+@section('pg-specific-js')
+<!-- Page specific javascripts-->
+<!-- Data table plugin-->
+<script type="text/javascript" src="{{ asset('vali/js/plugins/jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vali/js/plugins/dataTables.bootstrap.min.js') }}"></script>
+<script type="text/javascript">$('#sampleTable').DataTable();</script>
+<script>
+  $(document).ready(function(){
+    $('#li-maintenance').addClass('is-expanded');
+    $('a[href="/admin/maintenance/requirements"]').addClass('active');
+  });
+</script>
+<!-- Plugins for this page -->
+<!-- ============================================================== -->
+<!-- jQuery file upload -->
+<script src="{{ asset('elite/js/dropify.min.js') }}"></script>
+<script>
+$(document).ready(function() {
+    // Basic
+    $('.dropify').dropify();
+
+    // Translated
+    $('.dropify-fr').dropify({
+        messages: {
+            default: 'Glissez-déposez un fichier ici ou cliquez',
+            replace: 'Glissez-déposez un fichier ou cliquez pour remplacer',
+            remove: 'Supprimer',
+            error: 'Désolé, le fichier trop volumineux'
+        }
+    });
+
+    // Used events
+    var drEvent = $('#input-file-events').dropify();
+
+    drEvent.on('dropify.beforeClear', function(event, element) {
+        return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+    });
+
+    drEvent.on('dropify.afterClear', function(event, element) {
+        alert('File deleted');
+    });
+
+    drEvent.on('dropify.errors', function(event, element) {
+        console.log('Has Errors');
+    });
+
+    var drDestroy = $('#input-file-to-destroy').dropify();
+    drDestroy = drDestroy.data('dropify')
+    $('#toggleDropify').on('click', function(e) {
+        e.preventDefault();
+        if (drDestroy.isDropified()) {
+            drDestroy.destroy();
+        } else {
+            drDestroy.init();
+        }
+    })
+});
+</script>
+@endsection
